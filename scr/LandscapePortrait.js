c6559da0d9e1ce5d71339dc482822bf0e834be70
evanator05 <watsonevan2005@gmail.com>
2021-09-22 09:53:01 -0600

    webby on skoo time

--- scr/LandscapePortrait.js
+++ scr/LandscapePortrait.js
@@ -11,19 +11,24 @@ function main() {
   secondNumber = document.getElementById("textfield2").value;
   //validate by alert or HTML text in p-tag
 
-  console.log(Number(firstNumber)+Number(secondNumber))
-  if (Number(firstNumber) > Number(secondNumber)) {
-    document.getElementById("answer").innerHTML = "Landscape"
+  stop = ((testNaN(Number(firstNumber)) == true || testNaN(Number(secondNumber)) == true));
+
+  if (!stop) {
+    if (Number(firstNumber) > Number(secondNumber)) {
+      document.getElementById("answer").innerHTML = "Landscape";
+    } else {
+      document.getElementById("answer").innerHTML = "Portrait";
+    };
   } else {
-    document.getElementById("answer").innerHTML = "Portrait"
+    document.getElementById("answer").innerHTML = "Error";
+    alert("Please Enter 2 Real Numbers");
   };
 };
 
 function testNaN(number) {
     if (isNaN(number)) { //NaN are not values of real number system
-      stop = true;
-      return "Please type a real number";
+      return true;
     } else {
-      return "Input Validated";
+      return false;
     };
 };
