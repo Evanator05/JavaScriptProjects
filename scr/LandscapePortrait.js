a8f252a0e0e0fdb9816fa4a27e102233e909d061
evanator05 <watsonevan2005@gmail.com>
2021-09-22 09:24:31 -0600

    shtuff

--- scr/LandscapePortrait.js
+++ scr/LandscapePortrait.js
@@ -10,12 +10,9 @@ function main() {
   firstNumber = document.getElementById("textfield1").value;
   secondNumber = document.getElementById("textfield2").value;
   //validate by alert or HTML text in p-tag
-  alert(testNaN(firstNumber));
-  document.getElementById("validityTest1").innerHTML = testNaN(firstNumber);
-  alert(testNaN(secondNumber));
-  document.getElementById("validityTest2").innerHTML = testNaN(secondNumber);
-  console.log(firstNumber+secondNumber)
-  if (firstNumber > secondNumber) {
+
+  console.log(Number(firstNumber)+Number(secondNumber))
+  if (Number(firstNumber) > Number(secondNumber)) {
     document.getElementById("answer").innerHTML = "Landscape"
   } else {
     document.getElementById("answer").innerHTML = "Portrait"
