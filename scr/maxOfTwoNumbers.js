5419c49f48e025afc3766ed06281f361b082ef9c
evanator05 <watsonevan2005@gmail.com>
2021-09-23 09:28:47 -0600

    maxOf2Numbers

---
+++ scr/maxOfTwoNumbers.js
@@ -0,0 +1,17 @@
+let numberA = 0, numberB = 0;
+
+
+function main() {
+  let numberA, numberB;
+
+  numberA = document.getElementById("numberA").value;
+  numberB = document.getElementById("numberB").value;
+
+  numberA = Number(numberA);
+  numberB = Number(numberB);
+
+  document.getElementById("answer").innerHTML = numberA * (numberA > numberB) + numberB * (numberB > numberA) + (numberA+numberB) * (numberA==numberB);
+
+};
+
+main()
