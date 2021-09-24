ee9177430af20dcb718bdb274b80c3d83bc3784d
evanator05 <watsonevan2005@gmail.com>
2021-09-24 09:15:19 -0600

    Adding snake

--- scr/maxOfTwoNumbers.js
+++ scr/maxOfTwoNumbers.js
@@ -8,7 +8,10 @@ function main() {
   numberB = Number(numberB);
   //check if they are both real numbers
   if (!(isNaN(numberA) || isNaN(numberB))) {
-    document.getElementById("answer").innerHTML = numberA * (numberA > numberB) + numberB * (numberB > numberA) + (numberA+numberB) * (numberA==numberB);
+    document.getElementById("answer").innerHTML = numberA * (numberA > numberB) + numberB * (numberB > numberA);
+    if (numberA == numberB) {
+      document.getElementById("answer").innerHTML = "Equal"
+    };
   } else {
     document.getElementById("answer").innerHTML = "Please Input 2 Real Numbers"
   };
